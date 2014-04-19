0e7c44a9674e16351c38a39ef8d1202a219251ed
pragya5 <pragya.singh.mk@gmail.com>
2014-04-18 17:45:19 -0700

    Create AppDelegate.h

---
+++ AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Calculator
+//
+//  Created by Pragya Singh on 3/26/14.
+//  Copyright (c) 2014 . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
